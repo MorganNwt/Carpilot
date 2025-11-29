7d5aab7a45e2f6e2fa4700a91f3b6c394b28e5f9
morganNwt <metodik60@hotmail.fr>
2025-11-29 12:37:20 +0100

    feat(index) add partial astuces

--- src/Controller/Api/Admin/AgentController.php
+++ src/Controller/Api/Admin/AgentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Api\Admin;
 
-use App\DTO\User\Admin\UserResponseDto;
+use App\DTO\Admin\UserResponseDto;
 use App\Service\Admin\AgentService;
 use Nelmio\ApiDocBundle\Attribute\Model;
 use Nelmio\ApiDocBundle\Attribute\Security;
@@ -25,8 +25,7 @@ final class AgentController extends AbstractController
 
     public function __construct(
         private readonly AgentService $userService
-    ) {
-    }
+    ) {}
 
     #[Route('', name: 'list', methods: ['GET'])]
     #[OA\Get(
