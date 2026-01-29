99e92e6c65b990f6810923448d3ddc7176942b09
morganNwt <metodik60@hotmail.fr>
2026-01-29 17:26:17 +0100

    feat(OfferPriceDto): add regex

--- src/DTO/Estimation/OfferPriceDto.php
+++ src/DTO/Estimation/OfferPriceDto.php
@@ -6,7 +6,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class OfferPriceDto
 {
-    #[Assert\NotNull]
-    #[Assert\Positive]
-    public float $offer_price;
+    // REGEX : "12000", "12000.5", "12000.50"
+    #[Assert\NotBlank]
+    #[Assert\Regex(
+        pattern: '/^\d+(\.\d{1,2})?$/',
+        message: 'Invalid price format.'
+    )]
+    public string $offer_price;
 }
