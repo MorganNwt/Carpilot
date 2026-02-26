f1d090fe049afad407e32c1d9a38de44de985eb2
morganNwt <metodik60@hotmail.fr>
2026-02-26 14:11:02 +0100

    feat(dashboardController): delet IsGranted for admin and agent

--- src/Controller/Web/Admin/DashboardController.php
+++ src/Controller/Web/Admin/DashboardController.php
@@ -6,7 +6,7 @@ use App\Service\Admin\AdminDashboardService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 final class DashboardController extends AbstractController
 {
--- src/Controller/Web/Agent/DashboardController.php
+++ src/Controller/Web/Agent/DashboardController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Web\Agent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class DashboardController extends AbstractController
 {
